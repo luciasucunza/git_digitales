9a61d10fb711faf59cef0d50b16462105d4aef43
luciasucunza <sucunza.lucia@hotmail.com>
2018-11-27 12:45:00 -0300

    27Nov

--- GuiaDeClases04/guiaDeClase04_06.vhd
+++ GuiaDeClases04/guiaDeClase04_06.vhd
@@ -2,88 +2,49 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity myUartTest is
-    Port ( clk : in   STD_LOGIC;
-           rst : in   STD_LOGIC;
-           sw 	: in   STD_LOGIC_VECTOR (4-1 downto 0);
-           tx 	: out  STD_LOGIC;
-           led	: out  STD_LOGIC_VECTOR (8-1 downto 0);
-           rx 	: in   STD_LOGIC
-			  );
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           sw  : in  STD_LOGIC_VECTOR (8-1 downto 0);
+           tx  : out  STD_LOGIC;
+           led : out  STD_LOGIC_VECTOR (8-1 downto 0);
+           rx  : in  STD_LOGIC);
 end myUartTest;
 
-architecture ARCH_myUartTest of myUartTest is
+architecture Arch_myUartTest of myUartTest is
 
-	signal sDataRd   	: STD_LOGIC;
-	signal sDataRx   	: STD_LOGIC_VECTOR (8-1 downto 0);
-	signal sLedsNow  	: STD_LOGIC_VECTOR (8-1 downto 0);
-	signal sLedsNext	: STD_LOGIC_VECTOR (8-1 downto 0);
-	
-	signal sReady		: STD_LOGIC;
-	signal sDataWr		: STD_LOGIC;
-	signal sDataTxNow	: STD_LOGIC_VECTOR (4-1 downto 0);
-	signal sDataTxNext: STD_LOGIC_VECTOR (4-1 downto 0);
-	signal sDataTxFULL: STD_LOGIC_VECTOR (8-1 downto 0);
-	
+	signal sRx 		: STD_LOGIC;
+	signal ena		: STD_LOGIC;
+	signal ena_led	: STD_LOGIC;
+	signal sTx		: STD_LOGIC;
+	signal sLed 	: STD_LOGIC_VECTOR (8-1 downto 0);
 
 begin
-
-	SEC: PROCESS( clk )
-	begin
-		if rising_edge(clk) then
-			if rst = '1' then
-				sLedsNow 	<= (others => '0');
-				sDataTxNow <= "0000";
-			else
-				sLedsNow 	<= sLedsNext;
-				sDataTxNow  <= sDataTxNext;
-			end if;
-		end if;
+	sRx <= rx;
+	tx 	<= sTx;
+	led <= sLed when ena_led = '1' else (others => '0');
 	
-	end PROCESS;
-	
-	
-	LCRX: PROCESS( sDataRd, sDataRx, sLedsNow )
-	begin
-		if sDataRd = '1' then
-			sLedsNext 	<= sDataRx;
-		else
-			sLedsNext 	<= sLedsNow;
-		end if;
-	end PROCESS;
+	CONT: entity work.myCnt (Arch_myCnt)
+	generic map (M => 100000000) 
+	PORT MAP(
+		clk 		=> clk,
+		rst 		=> rst,
+		ena 		=> '1', 
+		salidaM_2 	=> open,
+		salidaM 	=> ena
+	);
 
+	UART: entity work.myUart (Arch_myUart)
+	PORT MAP(
+		clk 	=> clk,
+		rst 	=> rst,
+		dataWr 	=> ena,
+		dataTx 	=> sw,
+		ready 	=> open,
+		tx 		=> sTx,
+		dataRd 	=> ena_led,
+		dataRx 	=> sLed,
+		rx 		=> sRx
+	);
 
-	LC: PROCESS( sDataTxNow, sReady, sw )
-	begin
-		if sReady = '1' then
-			if sDataTxNow = sw then
-			else
-				sDataTxNext <= sw ;
-				sDataWr		<= '1';
-			end if;
-		else
-				sDataTxNow	<= sDataTxNext;
-				sDataWr		<= '0';
-		end if;
-	end PROCESS;
-	
-	
--- Si pongo el "0101" se infiere un latch 
---	sDataTxFULL <= sDataTxNow & "0101";
-	sDataTxFULL <= sDataTxNow & sDataTxNow;
-	led 			<= sLedsNow;
-
-
-	UART: entity WORK.myUart(ARCH_myUart)
-	generic MAP( dataSize => 8	 )	
-	port MAP( 	clk 	 => clk,
-					rst 	 => rst,
-					dataWr => sDataWr,
-					dataTx => sDataTxFULL,
-					ready  => sReady,
-					tx 	 => tx,		
-					dataRd => sDataRd,		
-					dataRx => sDataRx,		
-					rx		 => rx
-				);
 				
 end ARCH_myUartTest;
